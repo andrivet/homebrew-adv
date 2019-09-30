9c31b7123e809ffa34e27620075b568101c51537
Sebastien Andrivet <sebastien@andrivet.com>
2019-09-30 22:41:01 +0200

    Add SHA265 digest

--- Formula/sjasmplus.rb
+++ Formula/sjasmplus.rb
@@ -2,6 +2,7 @@ class Sjasmplus < Formula
     desc "sjasmplus: Z80 cross-assembler"
     homepage "https://github.com/andrivet/sjasmplus"
     url "https://github.com/andrivet/sjasmplus/archive/v1.14.1.tar.gz"
+    sha265 "0de2724f46357ad6da0072eb930301793491a33ead4dfa7e7ad0be1dfc0b902b"
     depends_on "cmake" => :build
     depends_on "boost"
   
