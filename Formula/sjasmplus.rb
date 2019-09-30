882504e15bf6b75661972ea123a839622f32d1c7
Sebastien Andrivet <sebastien@andrivet.com>
2019-09-30 22:38:56 +0200

    Switch to version from z00m128 forked in my repository

--- Formula/sjasmplus.rb
+++ Formula/sjasmplus.rb
@@ -1,8 +1,7 @@
 class Sjasmplus < Formula
-    desc "SJAsmPlus: Z80 cross-assembler"
-    homepage "https://github.com/sjasmplus/sjasmplus/wiki"
-    url "https://github.com/sjasmplus/sjasmplus/archive/20190306.1.tar.gz"
-    sha256 "f3f6d28af19880ed2cb427b6b427e9bd42371929c7d263dac840fb71de1302d6"
+    desc "sjasmplus: Z80 cross-assembler"
+    homepage "https://github.com/andrivet/sjasmplus"
+    url "https://github.com/andrivet/sjasmplus/archive/v1.14.1.tar.gz"
     depends_on "cmake" => :build
     depends_on "boost"
   
